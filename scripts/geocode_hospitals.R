ea0f87adbbfd03cce4fcbd4b2e3c00c1a50c7e5b
dfsnow <dfsnow@uchicago.edu>
2019-06-10 05:39:29 +0000

    Added hospital data

--- scripts/geocode_hospitals.R
+++ scripts/geocode_hospitals.R
@@ -2,5 +2,6 @@ library(tidyverse)
 library(ggmap)
 
 hospitals <- read_tsv("data/hospitals.tsv") %>% 
-  mutate_geocode(hospital_name)
+  mutate_geocode(hospital_name) %>%
+  write_csv("data/hospitals.csv")
 
